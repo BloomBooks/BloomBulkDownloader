54ecd2e8fe9725f77f8351a11102bc1650f923a9
Gordon Martin <gordon_martin@sil.org>
2017-08-16 14:57:49 -0700

    BL-4866 Bulk downloader
    * initial framework

---
+++ BloomBulkDownloader/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.Auth.AccessControlPolicy.ActionIdentifiers;
+using Amazon.Runtime.Internal.Auth;
+using Amazon.S3;
+
+namespace BloomBulkDownloader
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var s3 = new AmazonS3Client();
+		}
+	}
+}
