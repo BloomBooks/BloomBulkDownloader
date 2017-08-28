fed31614a122c6400e31a86a2c55242130b566d5
Gordon Martin <gordon_martin@sil.org>
2017-08-28 14:45:08 -0700

    Add failing test for duplicate books

--- BloomBulkDownloaderTests/DownloaderTests.cs
+++ BloomBulkDownloaderTests/DownloaderTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using BloomBulkDownloader;
 
@@ -8,7 +9,22 @@ namespace BloomBulkDownloaderTests
 	[TestFixture]
     public class DownloaderTests
 	{
-		private IEnumerable<DownloaderParseRecord> _testParseRecords;
+		private List<DownloaderParseRecord> _testParseRecords;
+		private readonly ParseUploader _testUploader1;
+		private readonly ParseUploader _testUploader2;
+		private readonly ParseLanguage _testLanguage;
+
+		public DownloaderTests()
+		{
+			_testUploader1 = new ParseUploader {Email = "somebody@gmail.com"};
+			_testUploader2 = new ParseUploader {Email = "somebodyelse@gmail.com"};
+			_testLanguage = new ParseLanguage
+			{
+				EthnologueCode = "eng",
+				IsoCode = "en",
+				Name = "English"
+			};
+		}
 
 	    [SetUp]
 	    public void TestSetup()
@@ -107,17 +123,54 @@ namespace BloomBulkDownloaderTests
 	    {
 			// Setup
 		    var opts = new BulkDownloadOptions { Bucket = BulkDownloadOptions.BucketCategory.production };
+		    SetupParseRecords();
 
 			// Use our pre-determined set of DownloaderParseRecords
 		    BulkDownload.GetParseDbBooks = TestParseDbDelegate;
 
 			// SUT
 		    var listOfBooks = BulkDownload.GetFilteredListOfBooksToCopy(opts);
+			Assert.That(listOfBooks.Keys.Count, Is.EqualTo(2));
+			Assert.That(listOfBooks.First().Value.Item1, Is.EqualTo(_testUploader1.Email));
+		    Assert.That(listOfBooks.First().Value.Item2, Is.EqualTo("Box test"));
+		    Assert.That(listOfBooks.Last().Value.Item1, Is.EqualTo(_testUploader2.Email));
+		    Assert.That(listOfBooks.Last().Value.Item2, Is.EqualTo("Other test"));
 	    }
 
-	    private IEnumerable<DownloaderParseRecord> TestParseDbDelegate(BulkDownloadOptions options)
+		private IEnumerable<DownloaderParseRecord> TestParseDbDelegate(BulkDownloadOptions options)
 	    {
 		    return _testParseRecords;
 	    }
-    }
+
+		private void SetupParseRecords()
+		{
+			var parseRec1 = new DownloaderParseRecord
+			{
+				InCirculation = true,
+				InstanceId = "09f5edbe-6259-471e-88ea-e409113ddfb3",
+				Title = "\nBox\ntest\n",
+				Uploader = _testUploader1,
+				Languages = new List<ParseLanguage> { _testLanguage }
+			};
+			_testParseRecords.Add(parseRec1);
+			var parseRec2 = new DownloaderParseRecord
+			{
+				InCirculation = true,
+				InstanceId = "0612f158-2143-4767-b8a0-b83b32266810",
+				Title = "Other test",
+				Uploader = _testUploader1,
+				Languages = new List<ParseLanguage> { _testLanguage }
+			};
+			_testParseRecords.Add(parseRec2);
+			var parseRec3 = new DownloaderParseRecord
+			{
+				InCirculation = true,
+				InstanceId = "0612f158-2143-4767-b8a0-b83b32266810", // same instance and title as record 2, different uploader
+				Title = "Other test",
+				Uploader = _testUploader2,
+				Languages = new List<ParseLanguage> { _testLanguage }
+			};
+			_testParseRecords.Add(parseRec3);
+		}
+	}
 }
